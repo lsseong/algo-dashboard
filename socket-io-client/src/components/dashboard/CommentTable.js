b11657018d98b956062b7062dc073263efac8ddd
lsseong <lsseong@hotmail.com>
2019-02-19 18:54:14 +0800

    Renamed files

---
+++ socket-io-client/src/components/dashboard/CommentTable.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import ReactTable from "react-table";
+
+export default class CommentTable extends Component {
+  constructor(props) {
+    super(props);
+    this.storecommentarr = [];
+  }
+
+  componentWillMount() {
+    if (this.props.type.length !== 0) {
+      this.storecommentarr.unshift(this.props.type);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.currentStrat !== nextProps.currentStrat) {
+      this.storecommentarr = [];
+    }
+    if (this.storecommentarr.length > 80) {
+      this.storecommentarr.pop();
+    }
+    var pt = this.storecommentarr.findIndex(i => i.time === nextProps.type.time);
+    if (this.props.type !== nextProps.type) {
+      if (pt !== -1) {
+        this.storecommentarr.splice(pt, 1, nextProps.type);
+      } else if (pt < 0) {
+        this.storecommentarr.unshift(nextProps.type);
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="small">
+        <ReactTable
+          data={this.storecommentarr}
+          //pageSize={this.storecommentarr.length}
+          columns={[
+            {
+              Header: "COMMENTS",
+              getHeaderProps: (state, rowInfo, column, instance) => ({
+                style: {
+                  fontWeight: "600",
+                  textAlign: "center"
+                }
+              }),
+              columns: [
+                {
+                  Header: "Time",
+                  accessor: "time",
+                  width: 100
+                },
+                {
+                  Header: "Comment",
+                  accessor: "comment"
+                }
+              ]
+            }
+          ]}
+          className="-striped -highlight table border round"
+          showPageSizeOptions={false}
+          defaultPageSize={this.props.numofRows}
+        />
+      </div>
+    );
+  }
+}
---
+++ socket-io-client/src/components/dashboard/OrderTable.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import ReactTable from "react-table";
+export default class OrderTable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.storeorderarr = [];
+  }
+  componentWillMount() {
+    if (this.props.type.length !== 0) {
+      this.storeorderarr.unshift(this.props.type);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.currentStrat !== nextProps.currentStrat) {
+      this.storeorderarr = [];
+    }
+    if (this.storeorderarr.length > 80) {
+      this.storeorderarr.pop();
+    }
+    var pt = this.storeorderarr.findIndex(i => i.clientOrderId === nextProps.type.clientOrderId);
+    if (this.props.type !== nextProps.type) {
+      if (pt !== -1) {
+        this.storeorderarr.splice(pt, 1, nextProps.type);
+      } else if (pt < 0) {
+        this.storeorderarr.unshift(nextProps.type);
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="small">
+        <ReactTable
+          data={this.storeorderarr}
+          columns={[
+            {
+              Header: "ORDERS",
+              getHeaderProps: (state, rowInfo, column, instance) => ({
+                style: {
+                  fontWeight: "600",
+                  textAlign: "center"
+                }
+              }),
+              columns: [
+                {
+                  Header: "Time",
+                  accessor: "time",
+                  minWidth: "10%"
+                },
+                {
+                  Header: "Client Order Id",
+                  accessor: "clientOrderId",
+                  minWidth: "10%"
+                },
+                {
+                  Header: "State",
+                  accessor: "state",
+                  minWidth: "10%"
+                },
+                {
+                  Header: "Symbol",
+                  accessor: "order.symbol",
+                  minWidth: "10%"
+                },
+                {
+                  Header: "Side",
+                  accessor: "order.side",
+                  minWidth: "10%"
+                },
+                {
+                  Header: "Quantity",
+                  accessor: "order.qty",
+                  minWidth: "10%"
+                },
+                {
+                  Header: "Type",
+                  accessor: "order.type",
+                  minWidth: "10%"
+                },
+                {
+                  Header: "Limit Price",
+                  accessor: "order.limitPrice",
+                  minWidth: "10%"
+                },
+                {
+                  Header: "Avg Price",
+                  accessor: "avgPrice",
+                  minWidth: "10%"
+                }
+              ]
+            }
+          ]}
+          //pageSizeOptions = {[100]}
+
+          defaultPageSize={this.props.numofRows}
+          showPageSizeOptions={false}
+          className="-striped -highlight table border round"
+        />
+      </div>
+    );
+  }
+}
